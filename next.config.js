ee12085025fbbf1c865cfa700e24bb614880f10b
Kyle Williams <kyle.williams2015@yahoo.com>
2022-10-25 21:53:56 -0400

    layout changess

--- next.config.js
+++ next.config.js
@@ -2,20 +2,20 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const securityHeaders = [
-  {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on',
-  },
-  {
-    key: 'X-XSS-Protection',
-    value: '1; mode=block',
-  },
-  {
-    key: 'X-Frame-Options',
-    value: 'SAMEORIGIN',
-  },
-];
+// const securityHeaders = [
+//   {
+//     key: 'X-DNS-Prefetch-Control',
+//     value: 'on',
+//   },
+//   {
+//     key: 'X-XSS-Protection',
+//     value: '1; mode=block',
+//   },
+//   {
+//     key: 'X-Frame-Options',
+//     value: 'SAMEORIGIN',
+//   },
+// ];
 
 module.exports = withBundleAnalyzer({
     trailingSlash: false,
